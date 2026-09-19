1e10791973d69ec56a6ac9abbca9ad70621a6d74
kosnvny <409132@niuitmo.ru>
2026-09-19 18:34:56 +0300

    EDA

--- lab1.py
+++ lab1.py
@@ -29,6 +29,32 @@ green = "\033[32m"
   purple = "\033[35m"
 """
 
+def parse_number(value):
+    if pd.isna(value):
+        return np.nan
+
+    value = str(value).strip().replace(",", "")
+
+    if value == "":
+        return np.nan
+
+    multiplier = 1
+
+    if value[-1].upper() == "K":
+        multiplier = 1_000
+        value = value[:-1]
+
+    elif value[-1].upper() == "M":
+        multiplier = 1_000_000
+        value = value[:-1]
+
+    elif value[-1].upper() == "B":
+        multiplier = 1_000_000_000
+        value = value[:-1]
+
+    return float(value) * multiplier
+
+
 def format_string(string, colour):
     print(colour + string + "\033[0m")
 
@@ -51,9 +77,20 @@ print(df.dtypes)
 
 print(df.isnull().sum())
 
-numeric_columns = ["Rating"]
+numeric_columns = [
+    "Active",
+    "Visits",
+    "Favourites",
+    "Likes",
+    "Dislikes"
+]
 
-df = df.dropna(subset=numeric_columns)
+for column in numeric_columns:
+    df[column] = df[column].apply(parse_number)
+
+df["Rating"] = pd.to_numeric(df["Rating"], errors="coerce")
+
+df = df.dropna(subset=numeric_columns + ["Rating"])
 
 print("Количество дубликатов:", df.duplicated().sum())
 
@@ -103,3 +140,47 @@ pd.set_option(
     None
 )
 
+train_df = x_train.copy()
+train_df["Visits"] = y_train
+format_string("Описательная статистика", "\033[35m")
+print(train_df.describe())
+
+# todo анализ на выбросы + ассиметрию + отличия признаков по масштабу
+train_df.hist(figsize=(14, 10), bins=30)
+plt.tight_layout()
+plt.show()
+# todo конец анализа
+
+# todo что-то не так с графиками
+plt.figure(figsize=(10, 5))
+sns.histplot(train_df["Visits"], bins=10, kde=True)
+plt.title("Распределение Visits (таргета) в обучающей выборке")
+plt.show()
+
+plt.figure(figsize=(10, 4))
+sns.boxplot(x=train_df["Visits"])
+plt.title("Boxplot Visits")
+plt.show()
+# todo конец неправильных графиков
+
+plt.figure(figsize=(10, 8))
+sns.heatmap(train_df.corr(), annot=True, cmap="coolwarm", fmt=".2f")
+plt.title("Корреляционная матрица TRAIN")
+plt.show()
+
+fig, axes = plt.subplots(2, 3, figsize=(16, 10))
+
+for i, feature in enumerate(features):
+    row = i // 3
+    col = i % 3
+
+    sns.scatterplot(data=train_df, x=feature, y="Visits", ax=axes[row, col])
+
+    axes[row, col].set_title(f"{feature} vs Visits")
+    axes[row, col].set_xlabel(feature)
+    axes[row, col].set_ylabel("Visits")
+
+axes[1, 2].remove()
+
+plt.tight_layout()
+plt.show()
